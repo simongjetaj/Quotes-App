87f9ea767759f5a3fb024059c1e71b55c3d67268
Simon Gjetaj <gjetajsimon@gmail.com>
2018-10-19 14:40:54 +0200

    Minor change

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  faQuoteLeft = faQuoteLeft;
+  faQuoteRight = faQuoteRight;
 
   constructor() { }
 
